9a0f08bd2242fa5951dff6cf4268d433f495a48e
[Pedro Scheeffer] <pedro.s.d.p@hotmail.com>
2022-11-07 20:15:48 -0300

    Saque node de persona

--- src/Library/Persona.cs
+++ src/Library/Persona.cs
@@ -6,8 +6,6 @@ namespace Library
     {
         public string nombre;
         public int edad;
-        public List<Node> hijos = new List<Node>();
-        
         public Persona(string nombre, int edad){
             this.nombre = nombre;
             this.edad = edad;
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -17,14 +17,14 @@ namespace Program
             Persona juana = new Persona("juana",5);
             Persona jesus = new Persona("jesus",2022);
             // ocho nodos con las personas
-            Node<Persona> jorge1 = new Node<Persona>(1,jorge);
-            Node<Persona> juan2 = new Node<Persona>(2,juan);
-            Node<Persona> julian3 = new Node<Persona>(3,julian);
-            Node<Persona> julieta4 = new Node<Persona>(4,julieta);
-            Node<Persona> jimena5 = new Node<Persona>(5,jimena);
-            Node<Persona> jose6 = new Node<Persona>(6,jose);
-            Node<Persona> juana7 = new Node<Persona>(7,juana);
-            Node<Persona> jesus8 = new Node<Persona>(8,jesus);
+            Node<Persona> jorge1 = new Node<Persona>(1, jorge);
+            Node<Persona> juan2 = new Node<Persona>(2, juan);
+            Node<Persona> julian3 = new Node<Persona>(3, julian);
+            Node<Persona> julieta4 = new Node<Persona>(4, julieta);
+            Node<Persona> jimena5 = new Node<Persona>(5, jimena);
+            Node<Persona> jose6 = new Node<Persona>(6, jose);
+            Node<Persona> juana7 = new Node<Persona>(7, juana);
+            Node<Persona> jesus8 = new Node<Persona>(8, jesus);
             //arbol
             jorge1.AddChildren(juan2);
             jorge1.AddChildren(julian3);
