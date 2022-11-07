264a853dbc77d27042db3a227f71c15c5d2bd8ec
[Pedro Scheeffer] <pedro.s.d.p@hotmail.com>
2022-11-07 19:52:17 -0300

    comentarios

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,6 +7,7 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            // ocho personas
             Persona jorge = new Persona("jorge",50);
             Persona juan = new Persona("juan",20);
             Persona julian = new Persona("julian",3);
@@ -15,6 +16,7 @@ namespace Program
             Persona jose = new Persona("jose",786876);
             Persona juana = new Persona("juana",5);
             Persona jesus = new Persona("jesus",2022);
+            // ocho nodos con las personas
             Node jorge1 = new Node(1,jorge);
             Node juan2 = new Node(2,juan);
             Node julian3 = new Node(3,julian);
@@ -23,7 +25,7 @@ namespace Program
             Node jose6 = new Node(6,jose);
             Node juana7 = new Node(7,juana);
             Node jesus8 = new Node(8,jesus);
-            
+            //arbol
             jorge1.AddChildren(juan2);
             jorge1.AddChildren(julian3);
 
@@ -35,6 +37,7 @@ namespace Program
             juana7.AddChildren(jesus8);
             
             // visitar el árbol aquí
+            Console.WriteLine(juana7);
         }
     }
 }
